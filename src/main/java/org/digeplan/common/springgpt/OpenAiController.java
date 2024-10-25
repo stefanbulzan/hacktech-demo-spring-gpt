34ac18e111fe0c1bc98d390901d96b87a42aa59f
Beni Caba <bcaba@avolvesoftware.com>
2024-10-25 21:24:41 +0300

    ChatClient example

---
+++ src/main/java/org/digeplan/common/springgpt/OpenAiController.java
@@ -0,0 +1,27 @@
+package org.digeplan.common.springgpt;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.messages.UserMessage;
+import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("openapi")
+public class OpenAiController {
+    private final ChatClient chatClient;
+
+    public OpenAiController(ChatClient.Builder chatBuilder) {
+        chatClient = chatBuilder.build();
+    }
+
+    @GetMapping
+    public String tellAJoke() {
+        return chatClient.prompt(new Prompt(List.of(new UserMessage("Tell me a joke"))))
+                .call()
+                .content();
+    }
+}
